79261883c01b1fbfe5496ada714411a394340d17
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-08-01 10:28:48 +0530

    update

--- commit-to-all.sh
+++ commit-to-all.sh
@@ -5,7 +5,7 @@ last_commit=`git log --format="%H" -n 1 | cat`
 
 current_branch=`git rev-parse --abbrev-ref HEAD -- | head -n 1`
 
-for branch infor branch in $(git for-each-ref --format='%(refname)' refs/heads/); do
+for branch in $(git for-each-ref --format='%(refname)' refs/heads/); do
 	git checkout $branch
 	git cherry-pick $last_commit
 done
