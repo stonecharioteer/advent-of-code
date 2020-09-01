b50881d9bacebde1df721e3d61392e8eb3fabfe3
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-09-01 21:30:35 +0530

    add solution for 2015.07

--- advent_of_code/y2015/day07.py
+++ advent_of_code/y2015/day07.py
@@ -36,10 +36,97 @@ x: 123
 y: 456
 In little Bobby's kit's instructions booklet (provided as your puzzle input), what signal is ultimately provided to wire a?
 
+
+--- Part Two ---
+Now, take the signal you got on wire a, override wire b to that signal, and reset the other wires (including wire a). What new signal is ultimately provided to wire a?
+
+
 """
 
 from typing import TextIO, Tuple
 
 
+def evaluate(a, b, operator):
+    if operator == "AND":
+        return a & b
+    elif operator == "OR":
+        return a | b
+    elif operator == "LSHIFT":
+        return a << b
+    elif operator == "RSHIFT":
+        return a >> b
+
+
 def run(inp: TextIO) -> Tuple[int, int]:
     """Day 7 solution"""
+    import copy
+    data_dict_1 = {}
+    for line in inp:
+        line = line.strip()
+        if line == "":
+            continue
+        lhs, rhs = line.split("->")
+        lhs = lhs.strip()
+        rhs = rhs.strip()
+        data_dict_1[rhs] = {
+            "expression": lhs,
+            "value": None if not lhs.isnumeric() else int(lhs)
+        }
+    data_dict_2 = copy.deepcopy(data_dict_1)
+
+    def compute(data_dict):
+        unsolved = True
+        i = 0
+        while unsolved:
+            for key in data_dict:
+                if data_dict[key]["value"] is not None:
+                    continue
+                expression = data_dict[key]["expression"]
+                if expression.isnumeric():
+                    print(expression)
+                    data_dict[key]["value"] = int(expression)
+                elif expression.startswith("NOT"):
+                    player = expression.replace("NOT", "").strip()
+                    val = data_dict[player]["value"]
+                    if val is not None:
+                        data_dict[key]["value"] = ~ val
+                else:
+                    if "AND" in expression:
+                        operator = "AND"
+                    elif "OR" in expression:
+                        operator = "OR"
+                    elif "LSHIFT" in expression:
+                        operator = "LSHIFT"
+                    elif "RSHIFT" in expression:
+                        operator = "RSHIFT"
+                    else:
+                        val = data_dict[expression]["value"]
+                        if val is not None:
+                            data_dict[key]["value"] = val
+                        continue
+
+                    player_a, player_b = expression.split(operator)
+                    player_a = player_a.strip()
+                    player_b = player_b.strip()
+
+                    if player_a.isnumeric():
+                        val_a = int(player_a)
+                    else:
+                        val_a = data_dict[player_a]["value"]
+
+                    if player_b.isnumeric():
+                        val_b = int(player_b)
+                    else:
+                        val_b = data_dict[player_b]["value"]
+                    if val_a is not None and val_b is not None:
+                        data_dict[key]["value"] = evaluate(val_a, val_b, operator)
+            unsolved = len([key for key in data_dict if data_dict[key]["value"] is None]) > 0
+            i += 1
+        return data_dict
+    data_dict_1 = compute(data_dict_1)
+    part_1 = data_dict_1["a"]["value"]
+    data_dict_2["b"]["value"] = part_1
+    data_dict_2 = compute(data_dict_2)
+    part_2 = data_dict_2["a"]["value"]
+
+    return (part_1, part_2)
