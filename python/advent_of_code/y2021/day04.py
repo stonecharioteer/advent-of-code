d56b36c245a0928fd1aef24366fbb008f0d09019
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-12-04 20:36:11 +0530

    PYTHON add solution for 2021 day 04 part 1 with tests

---
+++ python/advent_of_code/y2021/day04.py
@@ -0,0 +1,176 @@
+"""--- Day 4: Giant Squid ---
+
+You're already almost 1.5km (almost a mile) below the surface of the ocean,
+already so deep that you can't see any sunlight. What you can see, however, is
+a giant squid that has attached itself to the outside of your submarine.
+
+Maybe it wants to play bingo?
+
+Bingo is played on a set of boards each consisting of a 5x5 grid of numbers.
+Numbers are chosen at random, and the chosen number is marked on all boards on
+which it appears. (Numbers may not appear on all boards.) If all numbers in any
+row or any column of a board are marked, that board wins. (Diagonals don't
+count.)
+
+The submarine has a bingo subsystem to help passengers (currently, you and the
+giant squid) pass the time. It automatically generates a random order in which
+to draw numbers and a random set of boards (your puzzle input). For example:
+
+    7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+    22 13 17 11  0
+    8  2 23  4 24
+    21  9 14 16  7
+    6 10  3 18  5
+    1 12 20 15 19
+
+    3 15  0  2 22
+    9 18 13 17  5
+    19  8  7 25 23
+    20 11 10 24  4
+    14 21 16 12  6
+
+    14 21 17 24  4
+    10 16 15  9 19
+    18  8 23 26 20
+    22 11 13  6  5
+    2  0 12  3  7
+
+After the first five numbers are drawn (7, 4, 9, 5, and 11), there are no
+winners, but the boards are marked as follows (shown here adjacent to each
+other to save space):
+
+    22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+    8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+    21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+    6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+    1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+
+After the next six numbers are drawn (17, 23, 2, 0, 14, and 21), there are
+still no winners:
+
+    22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+    8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+    21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+    6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+    1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+
+Finally, 24 is drawn:
+
+    22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+    8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+    21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+    6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+    1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+
+At this point, the third board wins because it has at least one complete row or
+column of marked numbers (in this case, the entire top row is marked: 14 21 17
+24 4).
+
+The score of the winning board can now be calculated. Start by finding the sum
+of all unmarked numbers on that board; in this case, the sum is 188. Then,
+multiply that sum by the number that was just called when the board won, 24, to
+get the final score, 188 * 24 = 4512.
+
+To guarantee victory against the giant squid, figure out which board will win
+first. What will your final score be if you choose that board?
+
+{{problem_statement_2 | default("Paste Problem Part 2 here")}}
+"""
+from typing import Tuple, Iterable, List
+
+
+def run(inp: Iterable) -> Tuple[int, int]:
+    """Solution for 2021 day 4"""
+    data = inp.read().splitlines()
+    result = squidgame_bingo_score(data)
+    return result
+
+def squidgame_bingo_score(data) -> Tuple[int, int]:
+    """Function that solves the squid bingo board problem(s)"""
+    random_draw_order = [int(x) for x in data[0].strip().split(",")]
+    boards = construct_boards(data[2:])
+    part_1 = None
+    for draw in random_draw_order:
+        for board_index, board in enumerate(boards):
+            found_value = board.set_state(draw)
+            if found_value:
+                if board.has_won:
+                    # calculate the score of the board
+                    board_unmarked_sum = board.unmarked_sum 
+                    part_1 = board_unmarked_sum * draw
+            if part_1 is not None:
+                break
+        if part_1 is not None:
+            break
+
+    part_2 = 0
+    return (part_1, part_2)
+
+
+def construct_boards(board_data) -> List:
+    """Given a string that contains a collection of bingo boards, this function
+    returns a list of boards, which are a multi-dimensional array (5x5)"""
+    # boards are split by 2 new lines
+    boards_data = "\n".join(board_data).split("\n\n")
+    boards = [Board(board) for board in boards_data]
+    return boards
+
+
+class Board:
+    """a bingo board object"""
+
+    def __init__(self, board_string):
+        self.board_array = self.construct_board(board_string)
+        self.marked_array = [[False for _ in range(5)] for _ in range(5)]
+
+    def construct_board(self, board_string):
+        """Takes a bingo board string and constructs a 5x5 array"""
+        board_array = []
+        for line in board_string.split("\n"):
+            data = [int(x) for x in line.strip().split()]
+            board_array.append(data)
+        return board_array
+    
+    def __repr__(self):
+        # reconvert board to string
+        board_as_string = "\n\t".join([" ".join([str(x) for x in row]) for row in self.board_array])
+        marked_as_string = "\n\t".join([" ".join([str(x) for x in row]) for row in self.marked_array])
+        board_string = "Board:\n\t[\n\t{}\n\t]\n\t[\n\t{}\n\t]".format(board_as_string, marked_as_string)
+        return board_string
+
+   
+    def set_state(self, number) -> bool:
+        """checks if a number exists in the board, and if it does, marks it.
+        if the number exists, then it returns True, else False"""
+        for row, row_values in enumerate(self.board_array):
+            for col, value in enumerate(row_values):
+                if (value == number) and (self.marked_array[row][col] is False):
+                    self.marked_array[row][col] = True
+                    return True
+        return False
+
+    @property
+    def has_won(self) -> bool:
+        """Checks if the board is in a 'won' state"""
+        for row in range(5):
+            row_vals = self.marked_array[row]
+            won = all(row_vals)
+            if won:
+                return True
+        for col in range(5):
+            col_vals = [self.marked_array[x][col] for x in range(5)]
+            won = all(col_vals)
+            if won:
+                return True
+        return False
+    
+    @property
+    def unmarked_sum(self):
+        """Calculate the sum of all the unmarked numbers on the board"""
+        unmarked_sum = 0
+        for row, row_values in enumerate(self.board_array):
+            for col, value in enumerate(row_values):
+                if self.marked_array[row][col] == False:
+                    unmarked_sum += value
+        return unmarked_sum
--- python/tests/test_2021.py
+++ python/tests/test_2021.py
@@ -1,9 +1,11 @@
+import textwrap
+
 from advent_of_code.y2021.day03 import binary_diagonistic
+from advent_of_code.y2021.day04 import squidgame_bingo_score
 
 
 def test_2021_day03():
     """Tests that the simple test case for 2021 day 03 works"""
-    import textwrap
     data = textwrap.dedent(""" 
             00100
             11110
@@ -24,3 +26,35 @@ def test_2021_day03():
     assert isinstance(result, tuple)
     assert result[0] == 198, "Part 1 is wrong"
     assert result[1] == 230, "Part 2 is wrong"
+
+def test_2021_day04():
+    """Tests that the simple testcase for 2021 day 04 works"""
+    data = textwrap.dedent("""
+        7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+        22 13 17 11  0
+        8  2 23  4 24
+        21  9 14 16  7
+        6 10  3 18  5
+        1 12 20 15 19
+
+        3 15  0  2 22
+        9 18 13 17  5
+        19  8  7 25 23
+        20 11 10 24  4
+        14 21 16 12  6
+
+        14 21 17 24  4
+        10 16 15  9 19
+        18  8 23 26 20
+        22 11 13  6  5
+        2  0 12  3  7
+    """)
+    data = data.strip().split("\n")
+    result = squidgame_bingo_score(data)
+    assert isinstance(result, tuple)
+    assert result[0] == 4512, "Part 1 is wrong"
+    raise NotImplementedError("Part 2 is pending")
+
+
+
